9d38f9215936ca91c1ace4b630408f70ef331c16
Zeminaaa <zeman.mate@gmail.com>
2024-11-23 14:08:34 +0100

    Update 5.py

--- priklady/5.py
+++ priklady/5.py
@@ -1,3 +1,4 @@
+# V textové grafice vykresli n vnořených čtverců dle zadání (pro 
 velikost=int(input("zadej kolik vepsaných čtverců chceš: "))
 plny=(4*velikost)+1
 mezery=(3*velikost)+(velikost-3)
@@ -30,7 +31,9 @@ for i in range(velikost):
         print("*  ",end="")
     print("")
 
-
+# tohle printnulo svrchní polovinu bez středového řádku
+# tam kde se používá range posun, tak se střídavě printuje * a mezera.
+# tam kde se používá range plny a mezery, tak se printuje buď určitý počet mezer, nebo *, to se vždy děje uprostřed
 
 
 
@@ -44,6 +47,7 @@ for i in range(posun):
         print("*  ",end="")
 print("")
 
+# tohle printnulo středový řádek, zase nejdřív střídavě * a mezera, následně 1x*, poté zase střídavě mezera a *
 
 plny+=4
 mezery+=4
@@ -76,3 +80,4 @@ for i in range(velikost):
         print("   ",end="")
         print("*  ",end="")
     print("")
+    # tohle stejným principem printnulo spodní polovinu bez středového řádku
