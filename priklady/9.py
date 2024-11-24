4af8488772c41fe0075af1dbf76c71f6973a2f19
Zeminaaa <zeman.mate@gmail.com>
2024-11-24 11:45:23 +0100

    Add files via upload

--- priklady/9.py
+++ priklady/9.py
@@ -1,27 +1,21 @@
 # Napiš program, který načte ze vstupu textový řetězec obsahující číslo (například „568“) a převede ho na proměnnou typu int.
 # Nesmíš využít funkci int(). Představ si, že jsi člověk, který ji programuje. Můžeš například využít ascii tabulku a funkci ord(). 
-string=str(input("zadej číslo: "))
-delka=len(string)
+zadani=str(input("zadej číslo: "))
+delka=len(zadani)
 p=[]
-ascii_list=[]
+p_int=[]
 posun=0
 vysledek=0
-for charakter in string:
-    p.append(charakter)
+
+p=list(zadani)
 
 for charakter in p:
     temp=ord(charakter)
-    ascii_list.append(temp)
+    p_int.append(temp-48)
 
 for i in range(delka):
-    temp=float(chr(ascii_list[0+posun]))
-    vysledek+=(10**(delka-1))*temp
+    vysledek+=(10**(delka-1))*(p_int[posun])
     posun+=1
     delka-=1
-vysledek=round(vysledek)
+
 print(vysledek)
-
-
-# asi to nejde, na řádku 17 mám error, protože ascii tabulka vrací ve formě stringu a ne integeru.
-# když se místo float tam dá int tak to funguje, ale to nemůžu správně. bez toho to nefunguje protože mi nechce počítat se stringem.
-# jinak takhle to teda funguje :)
