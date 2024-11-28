6c1e3d5953da719cbdbdfbce385c4c9c1471a45c
Zeminaaa <zeman.mate@gmail.com>
2024-11-28 22:51:44 +0100

    optimalizace bubblesort.py

--- priklady2/bubblesort.py
+++ priklady2/bubblesort.py
@@ -6,16 +6,18 @@ while True:
     num=int(num)
     p.append(num)
 
+check_if_swapped=True
 
-
-for i in range(len(p)):
+while check_if_swapped:
     posun=0
+    check_if_swapped=False
     for i in range(len(p)-1):
         
         if p[posun]>p[posun+1]:
             temp=p[posun+1]
             p[posun+1]=p[posun]
             p[posun]=temp
+            check_if_swapped=True
         posun+=1
 
 
