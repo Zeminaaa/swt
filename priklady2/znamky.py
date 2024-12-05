2aaf77295ee79e07c2f325c02272a5e2fa31d93e
Zeminaaa <zeman.mate@gmail.com>
2024-12-05 16:02:35 +0100

    Update znamky.py

--- priklady2/znamky.py
+++ priklady2/znamky.py
@@ -15,27 +15,13 @@ znamky = {
 posun=0
 
 for jmeno,info in studenti.items():
-    
-
-        
 
     test=(znamky[info["id"]])
     znamka=(sum(test)/len(test))
 
-
-    vek=info["vek"]
- 
-    
-
     if info["vek"] not in  p:
-        
         p[info["vek"]]=[]
 
     p[info["vek"]].append((jmeno,znamka))
     
-    
-
-
-
-
 print(p)
