39476729e0ce5b699f7c978b08b46dd88bd26790
Zeminaaa <zeman.mate@gmail.com>
2024-12-10 17:38:17 +0100

    Add files via upload

--- priklady2/11.py
+++ priklady2/11.py
@@ -11,11 +11,15 @@ znamky = {"001": [1, 2, 1], "002": [3, 4, 5], "003": [1, 1, 2]}
 
 #{"Anna": [1, 2, 1], "Petr": [3, 4, 5], "Lucie": [1, 1, 2]} 
 
+#Rozšíření: Pokud se ID studenta ve slovníku znamky nenachází, přiřaďte prázdný seznam. 
 p={}
-
 
 for jmeno,idcko in studenti.items():
     p[jmeno]=jmeno
-    p[jmeno]=znamky[idcko]
+
+    if idcko in tuple(znamky):
+        p[jmeno]=znamky[idcko]
+    else:
+        p[jmeno]=[]
 
 print(p)
