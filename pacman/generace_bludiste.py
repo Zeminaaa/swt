d79e490786e7d93ae7fcbca73d3b8c5fbc95aa64
Zeminaaa <zeman.mate@gmail.com>
2024-12-12 20:06:24 +0100

    Update generace_bludiste.py

--- pacman/generace_bludiste.py
+++ pacman/generace_bludiste.py
@@ -34,30 +34,61 @@ def vytvor_seedy(mapa):
         for x in range(2,MAPA_SIRKA-2,2):
                 mapa[y][x]=POLICKO_SEED
 
+def vytvor_zdi(mapa):
+    pocet_seedu=0
+    for radek in bludiste:
+        for seed in radek:
+            if seed==POLICKO_SEED:
+                pocet_seedu+=1
+
+    check=True
+    while check:
+       
+        vybrany_seed=random.randint(0,pocet_seedu)  
+        temp=0
+        print(f"seed:{vybrany_seed}, temp:{temp}")
+        x=-1
+        y=-1
+        for radek in bludiste:
+            y+=1
+            for seed in radek:
+                x+=1
+                if seed==POLICKO_SEED:
+                    temp+=1
+                    if temp==vybrany_seed:
+                        souradnice_x=x
+                        souradnice_y=y
+                        break
+            x=-1
+        if temp==0:
+            check=False
+        if mapa[souradnice_y][souradnice_x]==POLICKO_SEED:
+            mapa[souradnice_y][souradnice_x]=POLICKO_ZED
+            smer=random.randint(0,3)
+            while True:
+                #0= nahoru, 1 = vpravo, 2= dolů, 3= vlevo
+                if smer==0:
+                    souradnice_y+=1
+                if smer==1:
+                    souradnice_x+=1
+                if smer==2:
+                    souradnice_y-=1
+                if smer==3:
+                    souradnice_x-=1
+                
+                if mapa[souradnice_y][souradnice_x]==POLICKO_ZED:
+                    break
+                else:
+                    mapa[souradnice_y][souradnice_x]=POLICKO_ZED
+
+
+
+
 
 
 
 bludiste = generuj_pole()
 vytvor_okraj(bludiste)
 vytvor_seedy(bludiste)
+vytvor_zdi(bludiste)
 vytiskni_pole(bludiste)
-
-
-pocet_seedu=0
-for radek in bludiste:
-    for seed in radek:
-        if seed==POLICKO_SEED:
-            pocet_seedu+=1
-
-vybrany_seed=random.randint(0,pocet_seedu)
-temp=0
-x=-1
-y=-1
-for radek in bludiste:
-    y+=1
-    for seed in radek:
-        x+=1
-        if seed==POLICKO_SEED:
-            pocet_seedu+=1
-            if temp==pocet_seedu:
-                print(bludiste[x][y])
